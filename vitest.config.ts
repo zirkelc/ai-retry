4e3b0d2155cf2fecdde3c10d83ebe95e8ec88245
zirkelc <chris.zirkel@gmail.com>
2025-11-26 07:32:49 +0100

    ci: add typecheck

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,8 @@ import { coverageConfigDefaults, defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     testTimeout: 120_000,
+    typecheck: {
+      enabled: true,
+    },
   },
 });
