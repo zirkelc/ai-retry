17cc785632f90d78bf2de1e02abfd0b9afcdbb28
zirkelc <chris.zirkel@gmail.com>
2025-11-28 13:57:11 +0100

    example

--- examples/ai-gateway.ts
+++ examples/ai-gateway.ts
@@ -1,18 +1,9 @@
 import { openai } from '@ai-sdk/openai';
-import { gateway, generateText } from 'ai';
+import { embed, gateway, generateText } from 'ai';
 import { createRetryable, EmbeddingModel } from '../src/index.js';
 
-const retryableLanguageModelDefault = createRetryable({
-  model: 'openai/gpt-4.1',
-  retries: [
-    'anthropic/claude-sonnet-4',
-    { model: 'xai/grok-4' },
-    gateway('openai/gpt-4.1'),
-  ],
-});
-
 const retryableLanguageModel = createRetryable({
-  model: gateway('openai/gpt-4.1'),
+  model: 'openai/gpt-4.1',
   retries: [
     'anthropic/claude-sonnet-4',
     { model: 'xai/grok-4' },
@@ -24,3 +15,17 @@ const retryableEmbeddingModel = createRetryable({
   model: gateway.textEmbeddingModel('openai/text-embedding-3-large'),
   retries: [],
 });
+
+const result = await generateText({
+  model: retryableLanguageModel,
+  prompt: 'What is the meaning of life?',
+});
+
+console.log(result.text);
+
+const embeddingResult = await embed({
+  model: retryableEmbeddingModel,
+  value: 'Hello world!',
+});
+
+console.log(embeddingResult.embedding);
